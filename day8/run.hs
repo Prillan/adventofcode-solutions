ba84b8956a6c3eea3788454d894e21971ce92510
Rasmus Précenth <prillan91@gmail.com>
2015-12-08 19:13:10 +0100

    Day8, nicer

--- day8/run.hs
+++ day8/run.hs
@@ -3,12 +3,10 @@ readString s = readString' . init . tail $ s
   where readHex x = read ("'\\x" ++ x ++ "'") :: Char
         readString' [] = []
         readString' [x] = [x]
-        readString' (x:y:xs)
-           | x == '\\' = case y of
-                           'x' -> (readHex (take 2 xs)):readString' (drop 2 xs)
-                           '\\' -> '\\':readString' xs
-                           '"' -> '"':readString' xs
-           | otherwise = x:readString' (y:xs)
+        readString' ('\\':'x':xs) =
+          readHex (take 2 xs):readString' (drop 2 xs)
+        readString' ('\\':y:xs) = y:readString' xs
+        readString' (x:xs) = x:readString' xs
 
 process input = (sum . map length $ ls) - (sum . map length . map readString $ ls)
   where ls = lines input
