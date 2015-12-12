5e320a1061bc73595ab5990f7965411ee776bb5a
Rasmus Précenth <prillan91@gmail.com>
2015-12-12 18:21:13 +0100

    Day12, both

---
+++ day12/hard.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Control.Monad ((>=>))
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import Data.Foldable
+import Data.Scientific
+
+nums :: Value -> [Scientific]
+nums (Object o) =
+  if String "red" `elem` o
+     then []
+     else foldl' (\acc v -> nums v ++ acc) [] o
+nums (Array a) = foldl' (\acc v -> nums v ++ acc) [] a
+nums (Number n) = pure n
+nums _ = []
+
+ints = map truncate
+
+process = decode >=> fmap (sum . ints . nums)
+
+main = do
+   input <- B.readFile "input.json"
+   print (process input)
