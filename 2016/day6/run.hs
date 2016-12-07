ea068ddb7a3135e1f9b40b67d0fb6f3b61903ec5
Rasmus Précenth <rasmus@precenth.eu>
2016-12-07 11:59:29 +0100

    Clean up 2016 day6 code

--- 2016/day6/run.hs
+++ 2016/day6/run.hs
@@ -1,19 +1,20 @@
 import           Data.List (sortOn)
-import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 
-inputWidth :: Int
-inputWidth = 8
-
+occurBy :: (Int -> Int) -> [String] -> String
 occurBy f =
   map (fst . head . sortOn (f . snd) . Map.toList)
   . foldr (zipWith update) empties
   where empties = repeat Map.empty
         update k m = Map.insertWith (+) k 1 m
 
+part1 :: [String] -> String
 part1 = occurBy negate
+
+part2 :: [String] -> String
 part2 = occurBy id
 
+main :: IO ()
 main = do
    input <- lines <$> readFile "input.txt"
    print (part1 input)
