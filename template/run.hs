303422c5ef7727b2e9b0284db14322206187ffd3
Rasmus Précenth <rasmus@precenth.eu>
2016-12-10 10:07:10 +0100

    Update template

--- template/run.hs
+++ template/run.hs
@@ -1,18 +1,17 @@
-import           Data.Aeson
 import           Data.List (permutations, group, minimum, maximum, minimumBy, maximumBy)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import           Text.Parsec
+import           Text.Megaparsec
 
 unsafeRight (Right x) = x
 
 parseAll = -- map unsafeRight .
   map (parse _ "") . lines
 
-part1 = _
-part2 = _
+part1 = id
+part2 = id
 
 main = do
    input <- parseAll <$> readFile "input.txt"
    print (part1 input)
-   print (part2 input)
+--   print (part2 input)
