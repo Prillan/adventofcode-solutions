52a8cfe694efeceb0729cc82b9526e9d35ac5a28
Rasmus Précenth <rasmus@precenth.eu>
2017-12-15 10:44:18 +0100

    2017, Day 15. Use Word64 instead of Integer.

--- 2017/day15/run.hs
+++ 2017/day15/run.hs
@@ -1,28 +1,29 @@
 import Data.Bits ((.&.))
+import GHC.Word (Word64)
 
-factorA, factorB :: Integer
+factorA, factorB :: Word64
 factorA = 16807
 factorB = 48271
 
-multiplesA, multiplesB :: Integer
+multiplesA, multiplesB :: Word64
 multiplesA = 4
 multiplesB = 8
 
-generate :: Integer -> Integer -> [Integer]
+generate :: Word64 -> Word64 -> [Word64]
 generate seed factor =
   iterate (\x -> (x * factor) `mod` (2 ^ 31 - 1)) seed
 
-equal :: [Integer] -> [Integer] -> [Bool]
+equal :: [Word64] -> [Word64] -> [Bool]
 equal genA genB = drop 1 $ zipWith (==) (map first16 genA) (map first16 genB)
-  where first16 = ((2^16 - 1) .&.)
+  where first16 = ((2 ^ 16 - 1) .&.)
 
-part1 :: Integer -> Integer -> Int
+part1 :: Word64 -> Word64 -> Int
 part1 seedA seedB = length
   . filter id
   . take (40 * 10^6)
   $ equal (generate seedA factorA) (generate seedB factorB)
 
-part2 :: Integer -> Integer -> Int
+part2 :: Word64 -> Word64 -> Int
 part2 seedA seedB = length
   . filter id
   . take (5 * 10^6)
@@ -30,7 +31,7 @@ part2 seedA seedB = length
           (filter (multipleOf multiplesB) (generate seedB factorB))
   where multipleOf n x = x `mod` n == 0
 
-
+main :: IO ()
 main = do
   let inputA = 591
       inputB = 393
