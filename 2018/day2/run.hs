6d6a5b95f35e97f52f4cd4f42c90ff170e26e7bd
Rasmus Précenth <rasmus@precenth.eu>
2018-12-03 09:59:14 +0100

    2018, day 2 and 3

---
+++ 2018/day2/run.hs
@@ -0,0 +1,50 @@
+import Data.Bits (xor)
+import Data.Char (ord)
+import Data.Maybe
+import Data.List
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+-- unsafeRight :: Show a => Either a b -> b
+-- unsafeRight (Right x) = x
+-- unsafeRight (Left x) = error $ show x
+
+replace m k f d =
+  let v = maybe d id (Map.lookup k m)
+  in Map.insert k (f v) m
+
+parseAll = lines
+
+part1 = uncurry (*) . foldl f (0, 0)
+  where f (d, t) i =
+          let counts = map snd . Map.toList $ countChars i
+          in
+            ( d + (if 2 `elem` counts then 1 else 0)
+            , t + (if 3 `elem` counts then 1 else 0))
+
+countChars :: String -> Map Char Int
+countChars = foldl go Map.empty
+  where go :: Map Char Int -> Char -> Map Char Int
+        go m c = replace m c (+1) 0
+
+part2 input =
+  let match = head
+            . filter ((== 1) . uncurry diff)
+            $ [(x, y) | x <- input, y <- input]
+
+  in
+    map fst $ filter (uncurry (==)) $ uncurry zip match
+  where diff :: String -> String -> Int
+        diff x y = sum $ zipWith f x y
+        f c d
+          | c == d    = 0
+          | otherwise = 1
+
+main = do
+   input <- parseAll <$> readFile "input.txt"
+   print (part1 input)
+   print (part2 input)
---
+++ 2018/day3/run.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE RecordWildCards #-}
+import Data.Maybe
+import Data.List
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+type Parser = Parsec (ErrorItem Char) String
+
+unsafeRight :: Show a => Either a b -> b
+unsafeRight (Right x) = x
+unsafeRight (Left x) = error $ show x
+
+type Grid = Map (Int, Int)
+
+replace f d m k  =
+  let v = maybe d id (Map.lookup k m)
+  in Map.insert k (f v) m
+
+data Claim = Claim { cId :: Int
+                   , cX  :: Int
+                   , cY  :: Int
+                   , cW  :: Int
+                   , cH  :: Int }
+  deriving (Show, Eq)
+
+int :: Parser Int
+int = read <$> many digitChar
+
+claimp = Claim
+  <$> (char '#' *> int)
+  <*> (string " @ " *> int)
+  <*> (char ',' *> int)
+  <*> (string ": " *> int)
+  <*> (char 'x' *> int)
+
+parseAll =
+  map unsafeRight .
+  map (parse claimp "") . lines
+
+cross :: [a] -> [b] -> [(a, b)]
+cross xs ys = (,) <$> xs <*> ys
+
+claimArea :: Claim -> Grid Int -> Grid Int
+claimArea Claim{..} m =
+  foldl (replace (+1) 0) m (cross [cX..cX+cW-1] [cY..cY+cH-1])
+
+part1 :: [Claim] -> Int
+part1 = length
+        . filter ((>1) . snd)
+        . Map.toList
+        . foldl (flip claimArea) Map.empty
+
+overlaps :: Claim -> Claim -> Bool
+overlaps c1 c2 =
+  let l1 = cX c1
+      r1 = cX c1 + cW c1
+      t1 = cY c1
+      b1 = cY c1 + cH c1
+      l2 = cX c2
+      r2 = cX c2 + cW c2
+      t2 = cY c2
+      b2 = cY c2 + cH c2
+  in
+    l1 < r2 && r1 > l2 && t1 < b2 && b1 > t2
+
+part2 :: [Claim] -> Int
+part2 input = cId . head . filter f $ input
+  where f c = all (not . overlaps c) $ filter ((/= cId c) . cId) input
+
+main :: IO ()
+main = do
+   input <- parseAll <$> readFile "input.txt"
+   print (part1 input)
+   print (part2 input)
