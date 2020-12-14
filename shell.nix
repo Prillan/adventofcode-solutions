36d5d45ee89f8b33cd82ed193562d28bf3c01017
Rasmus Précenth <rasmus@precenth.eu>
2020-12-14 20:37:47 +0100

    Add README

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
-{ nixpkgs ? import <nixpkgs> {} }:
+let
+  commit = "b94726217f7cdc02ddf277b65553762d520da196";
+  archive = "https://github.com/NixOS/nixpkgs/archive/" + commit + ".tar.gz";
+in { nixpkgs ? import (fetchTarball archive) { } }:
 let
   inherit (nixpkgs) haskellPackages;
-  aoc = haskellPackages.callPackage ./adventofcode/default.nix {  };
+  aoc = haskellPackages.callPackage ./adventofcode/default.nix { };
 in nixpkgs.mkShell {
-  buildInputs = [ (haskellPackages.ghcWithPackages (hpkgs: [hpkgs.split aoc])) ];
+  buildInputs =
+    [ (haskellPackages.ghcWithPackages (hpkgs: [ hpkgs.split aoc ])) ];
 }
