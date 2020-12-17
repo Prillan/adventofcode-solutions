615641b0cd620ed597e7c345e0f1e477761539cc
Rasmus Précenth <rasmus@precenth.eu>
2020-12-17 13:29:21 +0100

    2020, day 17 optimizations

--- 2020/day17/run.hs
+++ 2020/day17/run.hs
@@ -18,14 +18,18 @@ import qualified Data.Set as Set
 
 -- type Parser = Parsec (ErrorItem Char) String
 
-type G = Map (Int, Int, Int) Bool
+type G = Set (Int, Int, Int)
 
 parse '#' = True
 parse _ = False
 
 parseAll :: String -> G
 parseAll =
-  Map.mapKeys (\(x, y) -> (x, y, 0))
+  Set.fromList
+  . map (\(x, y) -> (x, y, 0))
+  . map fst
+  . filter snd
+  . Map.toList
   . parseMapGrid parse
 
 dirs :: [(Int, Int, Int)]
@@ -33,22 +37,22 @@ dirs =
   drop 1 $ (,,) <$> [0, -1, 1] <*> [0, -1, 1] <*> [0, -1, 1]
 
 neighbors g (x, y, z) =
-  mapMaybe (flip Map.lookup g)
+  filter (flip Set.member g)
   . map (\(dx, dy, dz) -> (x + dx, y + dy, z + dz))
   $ dirs
 
 step :: (Int, Int, Int) -> G -> ((Int, Int, Int), G)
 step (ix, iy, iz) g =
   let g' =
-        Map.fromList
-        . map (\pos -> (pos, rule g pos))
+        Set.fromList
+        . filter (rule g)
         $ (,,) <$> [-ix..ix] <*> [-iy..iy] <*> [-iz..iz]
   in ((ix + 1, iy + 1, iz + 1), g')
 
 rule :: G -> (Int, Int, Int) -> Bool
 rule g pos =
-  case (Map.lookup pos g, length . filter id . neighbors g $ pos) of
-    (Just True, alive)
+  case (Set.member pos g, length . neighbors g $ pos) of
+    (True, alive)
       | alive == 2 || alive == 3 -> True
       | otherwise -> False
     (_, 3) -> True
@@ -56,16 +60,20 @@ rule g pos =
 
 part1 initial =
   let (_, afterBoot) = iterate (uncurry step) ((8,8,1), initial) !! 6
-  in length . filter id $ Map.elems afterBoot
+  in Set.size afterBoot
 
 
 -- PART 2
 
-type G4 = Map (Int, Int, Int, Int) Bool
+type G4 = Set (Int, Int, Int, Int)
 
 parseAll4 :: String -> G4
 parseAll4 =
-  Map.mapKeys (\(x, y) -> (x, y, 0, 0))
+  Set.fromList
+  . map (\(x, y) -> (x, y, 0, 0))
+  . map fst
+  . filter snd
+  . Map.toList
   . parseMapGrid parse
 
 dirs4 :: [(Int, Int, Int, Int)]
@@ -73,22 +81,22 @@ dirs4 =
   drop 1 $ (,,,) <$> [0, -1, 1] <*> [0, -1, 1] <*> [0, -1, 1] <*> [0, -1, 1]
 
 neighbors4 g (x, y, z, w) =
-  mapMaybe (flip Map.lookup g)
+  filter (flip Set.member g)
   . map (\(dx, dy, dz, dw) -> (x + dx, y + dy, z + dz, w + dw))
   $ dirs4
 
 step4 :: (Int, Int, Int, Int) -> G4 -> ((Int, Int, Int, Int), G4)
 step4 (ix, iy, iz, iw) g =
   let g' =
-        Map.fromList
-        . map (\pos -> (pos, rule4 g pos))
+        Set.fromList
+        . filter (rule4 g)
         $ (,,,) <$> [-ix..ix] <*> [-iy..iy] <*> [-iz..iz] <*> [-iw..iw]
   in ((ix + 1, iy + 1, iz + 1, iw + 1), g')
 
 rule4 :: G4 -> (Int, Int, Int, Int) -> Bool
 rule4 g pos =
-  case (Map.lookup pos g, length . filter id . neighbors4 g $ pos) of
-    (Just True, alive)
+  case (Set.member pos g, length . neighbors4 g $ pos) of
+    (True, alive)
       | alive == 2 || alive == 3 -> True
       | otherwise -> False
     (_, 3) -> True
@@ -97,10 +105,10 @@ rule4 g pos =
 
 part2 initial =
   let (_, afterBoot) = iterate (uncurry step4) ((8,8,1,1), initial) !! 6
-  in length . filter id $ Map.elems afterBoot
+  in Set.size afterBoot
 
--- Runs in ~6s
--- ./run  6,45s user 0,04s system 99% cpu 6,498 total
+-- Runs in ~2s
+-- ./run  2,18s user 0,00s system 99% cpu 2,193 total
 main = do
   input <- readFile "input.txt"
   print (part1 $ parseAll input)
