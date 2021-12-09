082aee339e38d4b8101cfc02b3e2e7c78c8c0073
Rasmus Précenth <rasmus@precenth.eu>
2021-12-09 20:35:09 +0100

    2018, day11, massive performance improvements

--- 2018/day11/run.hs
+++ 2018/day11/run.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE BangPatterns #-}
 import Data.Ord (comparing)
-import Data.List (maximumBy)
-import Data.Vector (Vector)
-import qualified Data.Vector as V
+import Data.List (maximumBy, sortBy, transpose)
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V
 
 level :: Int -> Int -> Int -> Int
 level serial x y =
@@ -10,37 +10,77 @@ level serial x y =
   in
     ((((rackId * y + serial) * rackId) `div` 100) `mod` 10) - 5
 
-type Grid = Vector (Vector Int)
+type Grid = Vector Int
 
-initGrid :: Int -> Grid
+initGrid :: Int -> [[Int]]
 initGrid serial =
-  V.fromList [ V.fromList [ level serial x y | y <- [1..300] ] | x <- [1..300] ]
-
-square :: Int -> Int -> Int -> Grid -> Vector Int
-square s t l = V.concatMap (V.slice t s) . V.slice l s
+  [ [ level serial x y | x <- [1..300] ] | y <- [1..300] ]
 
 data Square = Square { left, top, power, size :: !Int }
-  deriving Show
+  deriving (Show, Eq)
 
-squares :: Grid -> Int -> [Square]
-squares grid s = do
-  l <- [1..300-s+1]
-  t <- [1..300-s+1]
-  pure $ Square { left  = l
-                , top   = t
-                , power = V.sum . square s (t-1) (l-1) $ grid
+flat :: [[Int]] -> Vector Int
+flat = V.fromList . concat
+
+lkp :: Grid -> Int -> Int -> Int -> Int
+lkp v n x y = v V.! (n * y + x)
+
+squares' :: Int -> Grid -> Int -> [Square]
+squares' n grid s = do
+  t <- [0..n - s]
+  l <- [0..n - s]
+  let b = t + s
+      r = l + s
+
+      -- Given a matrix (M) with submatrices A, B, C, D
+      -- / A B \
+      -- \ C D /
+      --
+      -- we have sum D = sum M - (sum A + sum B + sum C)
+      -- and we can get sum A = G_(l, t)
+      --                sum B = G_(l+s, t)
+      -- etc. to compute D without summing all elements
+
+      !sM  = lkp grid (n + 1) (l + s) (t + s)
+      !sAC = lkp grid (n + 1) l       (t + s)
+      !sAB = lkp grid (n + 1) (l + s) t
+      !sA  = lkp grid (n + 1) l       t
+
+  pure $ Square { left  = l + 1
+                , top   = t + 1
+                , power = sM + sA - sAB - sAC
                 , size  = s }
+
+
+example :: [[Int]]
+example =
+  [ [-2, -4,  4,  4,  4]
+  , [-4,  4,  4,  4, -5]
+  , [ 4,  3,  3,  4, -4]
+  , [ 1,  1,  2,  4, -3]
+  , [-1,  0,  2, -5, -2]
+  ]
+
+-- Computes rolling sums in both dimensions
+sums :: [[Int]] -> [[Int]]
+sums =
+  transpose
+  . map (scanl (+) 0)
+  . transpose
+  . map (scanl (+) 0)
 
 part1 :: Int -> Square
 part1 = maximumBy (comparing power)
-        . flip squares 3
+        . flip (squares' 300) 3
+        . flat
+        . sums
         . initGrid
 
 part2 :: Int -> Square
 part2 serial =
-  let grid = initGrid serial
+  let !grid = flat $ sums (initGrid serial)
   in maximumBy (comparing power)
-     . concatMap (squares grid)
+     . concatMap (squares' 300 grid)
      $ [1..300]
 
 result1 :: Square -> String
