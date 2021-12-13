cd6e0e7edaaa5e6d11f96a28c091e1432a2505e4
Rasmus Précenth <rasmus@precenth.eu>
2021-12-13 21:28:14 +0100

    2016, day23, much more performant, again

--- 2016/day23/run.hs
+++ 2016/day23/run.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+import           Data.Functor ((<&>))
 import           Data.List (permutations, group, minimum, maximum, minimumBy, maximumBy)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -10,7 +13,8 @@ import           Data.Void (Void)
 import           Text.Megaparsec hiding (State)
 import           Text.Megaparsec.Char
 
-type Reg = Char
+data Reg = A | B | C | D
+  deriving (Show, Eq)
 type Val = Int
 type ValReg = Either Val Reg
 data Instr = Cpy ValReg ValReg
@@ -21,68 +25,84 @@ data Instr = Cpy ValReg ValReg
            | Stop
   deriving (Show, Eq)
 
+type Parser a = Parsec Void String a
+
+numP :: Parser Int
 numP = read <$> ((++) <$> many (char '-') <*> some digitChar)
-regP = oneOf "abcd"
 
-valRegP = fmap Left numP <|> fmap Right regP
+regP :: Parser Reg
+regP = oneOf "abcd" <&> \case
+  'a' -> A
+  'b' -> B
+  'c' -> C
+  'd' -> D
 
+valRegP :: Parser ValReg
+valRegP = Left <$> numP <|> Right <$> regP
+
+cpyP :: Parser Instr
 cpyP =
   Cpy <$> (string "cpy " *> valRegP)
       <*> (string " " *> valRegP)
 
+jnzP :: Parser Instr
 jnzP =
   Jnz <$> (string "jnz " *> valRegP)
       <*> (string " " *> valRegP)
 
+incP :: Parser Instr
 incP =
   Inc <$> (string "inc " *> valRegP)
 
+decP :: Parser Instr
 decP =
   Dec <$> (string "dec " *> valRegP)
 
+tglP :: Parser Instr
 tglP =
   Tgl <$> (string "tgl " *> valRegP)
 
 instrP :: Parsec Void String Instr
 instrP = cpyP <|> jnzP <|> incP <|> decP <|> tglP
 
-
-data Zipper a = Z [a] a [a]
-  deriving Show
-
-instance Functor Zipper where
-  fmap f (Z l c r) = Z (f <$> l) (f c) (f <$> r)
-
--- current (Z _ c _) = c
-
-left (Z [] _ _) = undefined
-left (Z (l:ls) c rs) = Z ls l (c:rs)
-
-right (Z _ _ []) = undefined
-right (Z ls c (r:rs)) = Z (c:ls) r rs
-
-end (Z _ _ []) = True
-end (Z _ Stop _) = True
-end _ = False
-
--- fromList :: a -> [a] -> Zipper a
--- fromList def xs =
---   let (x':xs') = xs ++ repeat def
---   in Z (repeat def) x' xs'
-
-type Regs = Map Reg Val
+data Regs = Regs { regA, regB, regC, regD :: !Val }
 type State = (Regs, Int, Vector Instr)
 
-newState instr = ( Map.fromList (zip "abcd" (repeat 0))
+newState :: [Instr] -> State
+newState instr = ( emptyRegs
                  , 0
                  , V.fromList instr)
 
+emptyRegs :: Regs
+emptyRegs = Regs 0 0 0 0
+
+regVal :: Regs -> Reg -> Val
+regVal Regs {..} =
+  \case
+    A -> regA
+    B -> regB
+    C -> regC
+    D -> regD
+
+setReg :: Reg -> Val -> Regs -> Regs
+setReg r v regs =
+  case r of
+    A -> regs { regA = v }
+    B -> regs { regB = v }
+    C -> regs { regC = v }
+    D -> regs { regD = v }
+
+modifyReg :: (Val -> Val) -> Reg -> Regs -> Regs
+modifyReg f r regs@Regs {..} =
+  case r of
+    A -> regs { regA = f regA }
+    B -> regs { regB = f regB }
+    C -> regs { regC = f regC }
+    D -> regs { regD = f regD }
+
 val :: State -> Either Val Reg -> Val
 val _ (Left v) = v
-val (m, _, _) (Right r) = m Map.! r
-
-iter :: (a -> a) -> Int -> a -> a
-iter f n = foldr (.) id (replicate n f)
+val (m, _, _) (Right r) = regVal m r
 
 forward :: State -> Val -> State
 forward (r, !p, i) n = (r, p + n, i)
@@ -92,59 +112,52 @@ backward (r, !p, i) n = (r, p - n, i)
 
 jnz :: State -> Val -> Val -> State
 jnz s v steps
-  | v == 0               = s
-  | v /= 0 && steps >= 0 = forward s steps
-  | v /= 0 && steps <  0 = backward s (negate steps)
+  | v == 0     = s
+  | steps >= 0 = forward s steps
+  | steps <  0 = backward s (negate steps)
 
 cpy :: State -> Val -> Reg -> State
-cpy (!m, p, i) v k  = (Map.insert k v m, p, i)
+cpy (m, p, i) v k  = (setReg k v m, p, i)
 
 inc :: State -> Reg -> State
-inc (!m, p, i) k = (Map.update (pure . (+1)) k m, p, i)
+inc (m, p, i) k = (modifyReg (\v -> v + 1) k m, p, i)
 
 dec :: State -> Reg -> State
-dec (!m, p, i) k = (Map.update (pure . (+ (-1))) k m, p, i)
+dec (m, p, i) k = (modifyReg (\v -> v - 1) k m, p, i)
 
 tgl :: State -> Val -> State
-tgl s v
-  | abs v >= 30 = s
+tgl s@(m, p, i) n
+  | abs n >= 30 = s
   | otherwise =
-    let (d1, d2) =
-          if v >= 0
-            then (forward, backward)
-            else (backward, forward)
-        (m, p, i) = d1 s (abs v)
-        !i' = case i V.!? p of
-                Just instr -> V.modify (\v -> VM.write v p (tgl' instr)) i
+    let !i' = case i V.!? (p + n) of
+                Just instr -> V.modify (\v -> VM.write v (p + n) (tgl' instr)) i
                 _          -> i
-    in
-      d2 (m, p, i') (abs v)
+    in (m, p, i')
 
 tgl' :: Instr -> Instr
-tgl' Stop = Stop
-tgl' (Jnz v o) = Cpy v o
-tgl' (Cpy v t) = Jnz v t
-tgl' (Inc r) = Dec r
-tgl' (Dec r) = Inc r
-tgl' (Tgl v) = Inc v
+tgl' = \case Jnz v o -> Cpy v o
+             Cpy v t -> Jnz v t
+             Inc r -> Dec r
+             Dec r -> Inc r
+             Tgl v -> Inc v
+             Stop -> Stop
 
+current :: State -> Instr
 current (_, p, i) = maybe Stop id (i V.!? p)
 
-exec1 :: State -> State
-exec1 s =
-  let !s' = case current s of
-              Jnz v steps -> jnz s (val s v) ((val s steps)-1)
-              Cpy v (Right r) -> cpy s (val s v) r
-              Inc (Right r) -> inc s r
-              Dec (Right r) -> dec s r
-              Tgl v -> tgl s (val s v)
-              Stop -> backward s 1
-              _ -> s
-  in
-    forward s' 1
-
 exec :: State -> State
-exec = head . dropWhile ((/= Stop) . current) . iterate exec1
+exec = go
+  where go !s =
+          case current s of
+            Jnz v steps     -> continue $ jnz s (val s v) ((val s steps)-1)
+            Cpy v (Right r) -> continue $ cpy s (val s v) r
+            Inc (Right r)   -> continue $ inc s r
+            Dec (Right r)   -> continue $ dec s r
+            Tgl v           -> continue $ tgl s (val s v)
+            _               -> s
+
+        {-# INLINE continue #-}
+        continue s = go (forward s 1)
 
 unsafeRight (Right x) = x
 
@@ -153,8 +166,8 @@ parseAll = map unsafeRight .
 
 run input x =
   let (m, p, i) = newState input
-      (regs, _, _) = exec (Map.insert 'a' x m , p, i)
-  in regs Map.! 'a'
+      (regs, _, _) = exec (setReg A x m , p, i)
+  in regVal regs A
 
 part1 input = run input 7
 part2 input = run input 12
