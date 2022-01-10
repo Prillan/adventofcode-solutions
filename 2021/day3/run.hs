c383e852d24dadbd1ce598de50a61357e2a023d3
Rasmus Précenth <rasmus@precenth.eu>
2022-01-10 15:42:31 +0100

    2021, day3, "neater" solution

--- 2021/day3/run.hs
+++ 2021/day3/run.hs
@@ -4,19 +4,8 @@
 import AoC
 import AoC.Grid
 
-import Data.Bits (xor)
-import Data.Ord (comparing)
-import Data.Bifunctor
-import Data.Maybe
-import Data.List
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HashMap
-import Data.Sequence (Seq)
-import qualified Data.Sequence as Seq
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IntSet
+import Data.Maybe (mapMaybe)
+import Data.List (transpose)
 
 parseAll :: String -> [String]
 parseAll = lines
@@ -31,11 +20,15 @@ part1 :: [String] -> Int
 part1 input = gamma input * epsilon input
 
 locate :: ([Char] -> Char) -> [String] -> String
-locate selector = go 0
-  where go _ [x] = x
-        go i xs = let m = selector . map (!! i) $ xs
-                      rem = filter (\x -> x !! i == m) xs
-                  in go (i + 1) rem
+locate selector = go
+  where go = \case
+          [] -> []
+          xs -> let m = selector (map head xs)
+                in m:go (mapMaybe (pickSimilar m) xs)
+        pickSimilar m =
+          \case [_]           -> Nothing
+                x:xs | x == m -> Just xs
+                _             -> Nothing
 
 oxyGen :: [String] -> Int
 oxyGen = readBinary . locate mostCommon
@@ -46,9 +39,13 @@ co2Scrub = readBinary . locate leastCommon
 part2 :: [String] -> Int
 part2 input = oxyGen input * co2Scrub input
 
+main :: IO ()
 main = main' "input.txt"
+
+exampleMain :: IO ()
 exampleMain = main' "example.txt"
 
+main' :: FilePath -> IO ()
 main' file = do
    input <- parseAll <$> readFile file
    print (part1 input)
