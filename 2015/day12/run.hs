0e9244d5d8f0bc145f305bde69e61d75a04edeae
Rasmus Précenth <rasmus@precenth.eu>
2022-07-02 21:38:53 +0200

    2015, day 12 cleanup

--- 2015/day12/run.hs
+++ 2015/day12/run.hs
@@ -1,11 +1,30 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 import Data.Aeson
 import qualified Data.ByteString.Lazy as B
 
-nums (Object o) = foldl (\acc v -> nums v ++ acc) [] o
-nums (Array a) = foldl (\acc v -> nums v ++ acc) [] a
-nums (Number n) = pure n
-nums _ = []
+nums = go
+  where go = \case
+          Object o -> concatMap go o
+          Array a  -> concatMap go a
+          Number n -> pure n
+          _        -> []
 
-process x = sum . map truncate . nums <$> decode x
+part1 :: Value -> Int
+part1 = truncate . sum . nums
 
-main = B.readFile "input.json" >>= print . process
+nums' = go
+  where go = \case
+          Object o | String "red" `elem` o -> []
+                   | otherwise             -> concatMap go o
+          Array a  -> concatMap go a
+          Number n -> pure n
+          _        -> []
+
+part2 :: Value -> Int
+part2 = truncate . sum . nums'
+
+main = do
+  Just input <- decode <$> B.readFile "input.txt"
+  print (part1 input)
+  print (part2 input)
