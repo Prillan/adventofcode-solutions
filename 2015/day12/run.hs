79acb58bd0bff29142c0873efb627a237cf45b09
Rasmus Précenth <rasmus@precenth.eu>
2022-07-03 09:59:06 +0200

    2015, day 12, use Sum + foldMap
    Not much more readable, but a lot more fun!

--- 2015/day12/run.hs
+++ 2015/day12/run.hs
@@ -2,27 +2,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Data.Aeson
 import qualified Data.ByteString.Lazy as B
-
-nums = go
-  where go = \case
-          Object o -> concatMap go o
-          Array a  -> concatMap go a
-          Number n -> pure n
-          _        -> []
+import Data.Semigroup (Sum(..))
 
 part1 :: Value -> Int
-part1 = truncate . sum . nums
-
-nums' = go
+part1 = getSum . go
   where go = \case
-          Object o | String "red" `elem` o -> []
-                   | otherwise             -> concatMap go o
-          Array a  -> concatMap go a
-          Number n -> pure n
-          _        -> []
+          Object o -> foldMap go o
+          Array a  -> foldMap go a
+          Number n -> Sum $ truncate n
+          _        -> 0
 
 part2 :: Value -> Int
-part2 = truncate . sum . nums'
+part2 = getSum . go
+  where go = \case
+          Object o | String "red" `elem` o -> 0
+                   | otherwise             -> foldMap go o
+          Array a  -> foldMap go a
+          Number n -> Sum $ truncate n
+          _        -> 0
 
 main = do
   Just input <- decode <$> B.readFile "input.txt"
