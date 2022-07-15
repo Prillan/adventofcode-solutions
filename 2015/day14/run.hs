f0747cd03fd7eb2c378d19321cc701b912bfb6e7
Rasmus Précenth <rasmus@precenth.eu>
2022-07-15 17:46:32 +0200

    2015, day 14, cleanup

--- 2015/day14/run.hs
+++ 2015/day14/run.hs
@@ -1,48 +1,80 @@
-import Data.List (maximumBy, groupBy, sortBy)
-import Text.Parsec
-import qualified Data.MultiSet as MS
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeApplications #-}
 
-data Raindeer = RD String Integer Integer Integer
+import AoC
+import Data.Ord (comparing)
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Data.Map.Strict as Map
+
+type Parser = Parsec Void String
+
+data Raindeer = RD String Int Int Int
   deriving (Show, Eq, Ord)
 
-names = ["Dancer", "Cupid", "Rudolph", "Donner", "Dasher", "Blitzen", "Prancer", "Comet", "Vixen"]
+names :: [String]
+names = [ "Dancer"
+        , "Cupid"
+        , "Rudolph"
+        , "Donner"
+        , "Dasher"
+        , "Blitzen"
+        , "Prancer"
+        , "Comet"
+        , "Vixen"
+        ]
 
-number = (read :: String -> Integer) <$> many1 digit
+number :: Parser Int
+number = read <$> many digitChar
 
+raindeer :: Parser Raindeer
 raindeer = RD <$> (choice $ map (try.string) names)
               <*> (string " can fly " *> number)
               <*> (string " km/s for " *> number)
               <*> (string " seconds, but then must rest for " *> number)
 
-unsafeRight (Right x) = x
-
-distance :: Raindeer -> Integer -> Integer
+distance :: Raindeer -> Int -> Int
 distance (RD _ speed move rest) time = speed * (last + move*full)
   where (full, rest') = divMod time (move + rest)
         last = min move rest'
 
-distances max deer = map (\i -> map (\d -> (d, distance d i)) deer) [1..max]
+distances :: Int -> [Raindeer] -> [[(Raindeer, Int)]]
+distances maxDistance deer =
+  map (\i -> map (\d -> (d, distance d i)) deer) [1..maxDistance]
 
-maximumsBy f = last . groupBy (\a b -> f a b == EQ) . sortBy f
+maximumsBy :: (a -> a -> Ordering) -> [a] -> [a]
+maximumsBy _ [] = error "maximum of empty list"
+maximumsBy f (x:xs) = go [x] xs
+  where go acc@(a:_) =
+          \case []     -> acc
+                (b:bs) ->
+                  case f a b of
+                    LT -> go [b] bs
+                    EQ -> go (b:acc) bs
+                    GT -> go acc bs
 
+parseAll :: String -> [Raindeer]
+parseAll = unsafeRight . traverse (parse raindeer "") . lines
+
+unsafeRight (Right x) = x
+
+part1 :: [Raindeer] -> Int
 part1 = maximum
         . map (flip distance 2503)
-        . map unsafeRight
-        . map (parse raindeer "") . lines
 
-part2 = snd
-        . maximumBy (\a b -> snd a `compare` snd b)
-        . MS.toOccurList
-        . MS.fromList
-        . map (\(d, dist) -> (d, 1))
+part2 :: [Raindeer] -> Int
+part2 = maximum
+        . map snd
+        . Map.toList
+        . counter
+        . map fst
         . concat
-        . map (maximumsBy (\a b -> snd a `compare` snd b))
+        . map (maximumsBy (comparing snd))
         . distances 2503
-        . map unsafeRight
-        . map (parse raindeer "") . lines
 
 
 main = do
-   input <- readFile "input.txt"
+   input <- parseAll <$> readFile "input.txt"
    print (part1 input)
    print (part2 input)
