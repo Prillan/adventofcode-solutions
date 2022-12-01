babde85e2b5c44499a946ebb272ac6e48d3a19b5
Rasmus Précenth <rasmus@precenth.eu>
2022-12-01 14:26:25 +0100

    2022, day1, handle ridiculously large files

--- 2022/day1/run.hs
+++ 2022/day1/run.hs
@@ -1,20 +1,47 @@
-import Data.List (sortOn)
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
+import qualified Data.ByteString.Lazy as BL
+import Data.List (sort)
 import Data.List.Split (splitOn)
+import Data.Text.Encoding (decodeUtf8)
+import Data.Text.Read (decimal)
 
-parseAll :: String -> [[Int]]
-parseAll = map (map read) . splitOn [""] . lines
+largest3 :: [Int] -> [Int]
+largest3 = \case a:b:c:rest ->
+                   let [a',b',c'] = sort [a,b,c]
+                   in go a' b' c' rest
+                 xs -> sort xs
+  where -- a <= b <= c
+        go a b c [] = [a,b,c]
+        go !a !b !c (!x:xs)
+          | x >= c    = go b c x xs
+          | x >= b    = go b x c xs
+          | x >= a    = go x b c xs
+          | otherwise = go a b c xs
 
-part1 :: [[Int]] -> Int
-part1 = maximum . map sum
+parse :: BL.ByteString -> Int
+parse =
+  (\case (Right (x, _)) -> x)
+  . decimal
+  . decodeUtf8
+  . BL.toStrict
 
-part2 :: [[Int]] -> Int
-part2 = sum . take 3 . sortOn negate . map sum
+parseAll :: BL.ByteString -> [[Int]]
+parseAll =
+  map (map parse)
+  . filter (not . null)
+  . splitOn [BL.empty]
+  . BL.split 0x0a
 
 main :: IO ()
-main = main' "input.txt"
+main =
+  main' "input.txt"
+
+process :: BL.ByteString -> [Int]
+process = largest3 . map sum . parseAll
 
 main' :: FilePath -> IO ()
 main' file = do
-   input <- parseAll <$> readFile file
-   print (part1 input)
-   print (part2 input)
+   [x1, x2, x3] <- process <$> BL.readFile file
+   print $ x3
+   print $ x1 + x2 + x3
