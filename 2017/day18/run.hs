78684438a818923f4700be295a1d5ab2ab802fb6
Rasmus Précenth <rasmus@precenth.eu>
2023-01-10 20:48:16 +0100

    2017, day18 cleanup

--- 2017/day18/run.hs
+++ 2017/day18/run.hs
@@ -1,21 +1,17 @@
-import Data.Foldable
-import Data.Bits (xor)
-import Data.Maybe
-import Data.List
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HashMap
-import Data.Sequence ( Seq(..)
-                     , (|>)
-                     , viewr
-                     , ViewR(..)
-                     , viewl
-                     , ViewL(..)
-                     , dropWhileL )
-import qualified Data.Sequence as Seq
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Control.Monad.State
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+import Control.Monad (when)
+import Control.Monad.State (State, execState, get, gets, modify', put)
 import Data.Void (Void)
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Sequence ( Seq
+                     , (|>)
+                     , viewl
+                     , ViewL(..) )
+import qualified Data.Sequence as Seq
 import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
 
@@ -26,52 +22,60 @@ unsafeRight (Right x) = x
 unsafeRight (Left x) = error $ show x
 
 type Reg = Char
-data Action = Play Integer | Receive Integer
+data ProcessState = Receiving
+                  | Running
+                  | Sending Integer
   deriving Show
-data ProgramState = PState { program       :: HashMap Int (Program ())
+data ProgramState = PState { program       :: IntMap (Program ())
                            , pointer       :: Int
                            , programLength :: Int
-                           , regs          :: HashMap Reg Integer
-                           , actionLog     :: [Action] }
-
-instance Show ProgramState where
-  show s = "State { pointer = " ++ show (pointer s) ++ ", "
-           ++ "regs = " ++ show (regs s) ++ ", "
-           ++ "log = " ++ show (toList $ actionLog s) ++ " }"
+                           , regs          :: Map Reg Integer
+                           , queue         :: Seq Integer
+                           , processState  :: ProcessState
+                           , sentCount     :: Integer
+                           }
 
 type Program = State ProgramState
 
-append :: Action -> ProgramState -> ProgramState
-append a s = s { actionLog = a:actionLog s }
 
-isPlay (Play _) = True
-isPlay _ = False
+setState :: ProcessState -> ProgramState -> ProgramState
+setState p s = s { processState = p }
 
-receive :: Program (Maybe Integer)
-receive = do
-  actions <- gets actionLog
-  case filter isPlay actions of
-    Play freq:_  -> pure $ Just freq
-    _ -> pure $ Nothing
+append :: Integer -> ProgramState -> ProgramState
+append a s = s { queue = queue s |> a }
+
+dequeue :: Program (Maybe Integer)
+dequeue = do
+  s <- get
+  case viewl (queue s) of
+    x :< rest -> do
+      put s { queue = rest }
+      pure (Just x)
+    _ ->
+      pure Nothing
+
+receive :: Reg -> Program ()
+receive r = dequeue >>= \case
+  Just i -> do
+    modify' (setReg r i)
+    modify' (setState Running)
+  Nothing ->
+    modify' (setState Receiving)
 
 setReg :: Reg -> Integer -> ProgramState -> ProgramState
-setReg r v s = s { regs = HashMap.insert r v (regs s) }
+setReg r v s = s { regs = Map.insert r v (regs s) }
 
-regLookup :: Reg -> HashMap Reg Integer -> Integer
-regLookup r = maybe 0 id . HashMap.lookup r
+regLookup :: Reg -> Map Reg Integer -> Integer
+regLookup = Map.findWithDefault 0
 
+reg :: Reg -> ProgramState -> Integer
 reg r = regLookup r . regs
 
-play :: Reg -> Program ()
-play r = do
-  rv <- gets (reg r)
-  modify' (append (Play rv))
+send :: Integer -> Program ()
+send i = modify' (setState $ Sending i)
 
 numP :: Parser Integer
-numP = do
-  sign <- maybe 1 (const $ -1) <$> optional (char '-')
-  num <- read <$> some digitChar
-  pure $ sign * num
+numP = read <$> ((++) <$> many (char '-') <*> some digitChar)
 
 regValP :: Parser (Program Integer)
 regValP = (pure <$> numP) <|> (gets . reg <$> asciiChar)
@@ -83,62 +87,52 @@ operation :: (Integer -> Integer -> Integer)
 operation op x y = do
   vx <- gets (reg x)
   vy <- y
-  modify' (setReg x (vx `op` vy))
+  modify' $ setReg x (vx `op` vy)
 
 operationP :: (Integer -> Integer -> Integer)
            -> String
            -> Parser (Program ())
 operationP op s = do
-  string s
-  spaceChar
+  _ <- string s
+  _ <- spaceChar
   x <- asciiChar
-  spaceChar
+  _ <- spaceChar
   y <- regValP
   pure $ operation op x y
 
 sndP :: Parser (Program ())
 sndP = do
-  string "snd "
-  reg <- asciiChar
-  pure $ play reg
+  _ <- string "snd "
+  reg <- regValP
+  pure $ reg >>= send
 
 rcvP :: Parser (Program ())
 rcvP = do
-  string "rcv "
-  x <- regValP
-  pure $ do
-    vx <- x
-    if vx /= 0
-      then do
-        val <- receive
-        case val of
-          Just f -> modify' (append (Receive f))
-          Nothing -> error "No previous sound played"
-      else pure ()
+  _ <- string "rcv "
+  x <- asciiChar
+  pure $ receive x
 
 jgzP :: Parser (Program ())
 jgzP = do
-  string "jgz "
+  _ <- string "jgz "
   x <- regValP
-  spaceChar
+  _ <- spaceChar
   y <- regValP
-  pure $ do
+  pure do
     vx <- x
-    if vx > 0
-      then do
-        vy <- y
-        modify (\s -> s { pointer = pointer s + fromInteger vy - 1 })
-      else pure ()
+    when (vx > 0) do
+      vy <- y
+      modify' (\s -> s { pointer = pointer s + fromInteger vy - 1 })
 
 setP :: Parser (Program ())
 setP = do
-  string "set "
+  _ <- string "set "
   x <- asciiChar
-  spaceChar
+  _ <- spaceChar
   y <- regValP
-  pure $ do
+  pure do
     vy <- y
-    modify (setReg x vy)
+    modify' (setReg x vy)
 
 addP :: Parser (Program ())
 addP = operationP (+) "add"
@@ -149,36 +143,79 @@ mulP = operationP (*) "mul"
 modP :: Parser (Program ())
 modP = operationP mod "mod"
 
-parseInstruction = sndP <|> setP <|> addP <|> mulP <|> modP <|> rcvP <|> jgzP
+parseInstruction :: Parser (Program ())
+parseInstruction =
+  choice [ sndP
+         , setP
+         , addP
+         , mulP
+         , modP
+         , rcvP
+         , jgzP
+         ]
 
+parseAll :: String -> [Program ()]
 parseAll =
-  map unsafeRight .
-  map (parse parseInstruction "") . lines
+  map unsafeRight
+  . map (parse parseInstruction "")
+  . lines
 
-eval :: Program (Maybe Integer)
-eval = do
-  s <- get
-  case HashMap.lookup (pointer s) (program s) of
+eval :: (ProgramState, ProgramState) -> (Bool, (ProgramState, ProgramState))
+eval (p1, p2) =
+  case (processState p1, processState p2) of
+    (Receiving, Receiving) -> (False, (p1, p2))
+    (Running,   _) -> (True, (stepProcess p1, p2))
+    (Sending _, _) -> (True, send' p1 p2)
+    (_, Sending _) -> let (p2', p1') = send' p2 p1
+                      in
+                        (True, (p1', p2'))
+    (_, _)   -> (True, (p1, stepProcess p2))
+
+send' :: ProgramState -> ProgramState -> (ProgramState, ProgramState)
+send' p1 p2 =
+  case processState p1 of
+    Sending i -> ( setState Running p1 { sentCount = sentCount p1 + 1 }
+                 , setState Running (append i p2))
+    _ -> (p1, p2)
+
+stepProcess :: ProgramState -> ProgramState
+stepProcess p = flip execState p do
+  case IntMap.lookup (pointer p) (program p) of
     Just instr -> do
       instr
-      s' <- get
-      case actionLog s' of
-        Receive f:_ -> pure (Just f)
-        _ -> put s' { pointer = pointer s' + 1 } >> eval
-    Nothing -> pure Nothing
+      p' <- get
+      case processState p' of
+        Receiving -> pure ()
+        _ -> put p' { pointer = pointer p' + 1 }
+    Nothing -> pure ()
 
-part1 :: [Program ()] -> Maybe Integer
-part1 instr =
-  let initial = PState { pointer = 0
-                       , program = HashMap.fromList (zip [0..] instr)
-                       , programLength = length instr
-                       , regs = HashMap.empty
-                       , actionLog = [] }
-  in
-    fst $ runState eval initial
---part2 = id
+initial :: [Program ()] -> Integer -> ProgramState
+initial prog pid = PState { pointer = 0
+                          , program = IntMap.fromList (zip [0..] prog)
+                          , programLength = length prog
+                          , regs = Map.singleton 'p' pid
+                          , queue = Seq.empty
+                          , processState = Running
+                          , sentCount = 0 }
 
+part1 :: [Program ()] -> Integer
+part1 instr = go 0 $ initial instr 0
+  where go freq state =
+          let state' = stepProcess state
+          in case processState state' of
+            Running   -> go freq state'
+            Sending f -> go f state'
+            Receiving -> freq
+
+part2 :: [Program ()] -> Integer
+part2 instr = go (initial instr 0, initial instr 1)
+  where go ps =
+          case eval ps of
+            (False, (_, p2)) -> sentCount p2
+            (True, ps') -> go ps'
+
+main :: IO ()
 main = do
-   input <- parseAll <$> readFile "input.txt"
-   print (part1 input)
---   print (part2 input)
+  input <- parseAll <$> readFile "input.txt"
+  print $ part1 input
+  print $ part2 input
